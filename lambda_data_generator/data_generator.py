87d4befcbf2dfe296a8dd1e6f5f992f3ad1e53d4
Dorian <dorianteffo@gmail.com>
2024-04-09 11:17:28 +0200

    edit lambda

--- lambda_data_generator/data_generator.py
+++ lambda_data_generator/data_generator.py
@@ -75,7 +75,7 @@ def upload_to_s3(bucket_name: str,
     s3 = boto3.client('s3')
 
     s3.put_object(Bucket=bucket_name, Key=key, Body=data_string)
-    
+
 
 def lambda_handler(event, context):
     num_games = 10
