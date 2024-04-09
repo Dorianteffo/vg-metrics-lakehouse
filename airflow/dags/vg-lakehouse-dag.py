65f1c4571971da6643fc5100e6f4887b3cfb8e94
Dorian <dorianteffo@gmail.com>
2024-04-09 21:40:31 +0200

    add aws conn id second task group

--- airflow/dags/vg-lakehouse-dag.py
+++ airflow/dags/vg-lakehouse-dag.py
@@ -66,33 +66,35 @@ def lakehouse_dag():
 
         upload_bronze_job_s3 >> upload_silver_job_s3 >> upload_gold_job_s3
 
-    submit_glue_bronze_job = GlueJobOperator(
-        task_id="bronze-layer-job",
-        job_name=bronze_glue_job,
-        script_location=f"s3://{glue_bucket}/{bronze_glue_job_key}",
-        s3_bucket=glue_bucket,
-        iam_role_name=glue_iam_role,
-        create_job_kwargs=glue_args
-    )
 
-    submit_glue_silver_job = GlueJobOperator(
-        task_id="silver-layer-job",
-        job_name=silver_glue_job,
-        script_location=f"s3://{glue_bucket}/{silver_glue_job_key}",
-        s3_bucket=glue_bucket,
-        iam_role_name=glue_iam_role,
-        create_job_kwargs=glue_args
-    )
 
-    submit_glue_gold_job = GlueJobOperator(
-        task_id="gold-layer-job",
-        job_name=gold_glue_job,
-        script_location=f"s3://{glue_bucket}/{gold_glue_job_key}",
-        s3_bucket=glue_bucket,
-        iam_role_name=glue_iam_role,
-        create_job_kwargs=glue_args
-    )
+    @task_group(group_id='run_glue_jobs',
+                default_args={"aws_conn_id": "aws_conn", 
+                              "iam_role_name": glue_iam_role ,
+                              " create_job_kwargs" : glue_args,
+                              "s3_bucket" : glue_bucket}
+                )
+    def task_group_run_job():
+        submit_glue_bronze_job = GlueJobOperator(
+            task_id="bronze-layer-job",
+            job_name=bronze_glue_job,
+            script_location=f"s3://{glue_bucket}/{bronze_glue_job_key}"
+        )
 
-    task_group_upload_toS3() >> submit_glue_bronze_job >> submit_glue_silver_job >> submit_glue_gold_job
+        submit_glue_silver_job = GlueJobOperator(
+            task_id="silver-layer-job",
+            job_name=silver_glue_job,
+            script_location=f"s3://{glue_bucket}/{silver_glue_job_key}"
+        )
+
+        submit_glue_gold_job = GlueJobOperator(
+            task_id="gold-layer-job",
+            job_name=gold_glue_job,
+            script_location=f"s3://{glue_bucket}/{gold_glue_job_key}"
+        )
+        submit_glue_bronze_job >> submit_glue_silver_job >> submit_glue_gold_job
+
+
+    task_group_upload_toS3() >> task_group_run_job()
 
 lakehouse_dag()
