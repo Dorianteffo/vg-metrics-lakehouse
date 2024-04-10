d1c8ea797b0f604edccfccb4bc0b907ae7aabb3f
Dorian <dorianteffo@gmail.com>
2024-04-10 11:07:48 +0200

    use aws cli for gluejob

--- airflow/dags/vg-lakehouse-dag.py
+++ airflow/dags/vg-lakehouse-dag.py
@@ -1,8 +1,7 @@
 from datetime import datetime, timedelta
-from airflow.providers.amazon.aws.operators.glue import AwsGlueJobOperator
 from airflow.decorators import dag, task_group
+from airflow.providers.amazon.aws.operators.bash import BashOperator
 from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
-
 
 glue_bucket = "vg-lakehouse-glue"
 bronze_glue_job = "bronze-layer-job"
@@ -14,27 +13,7 @@ gold_glue_job_key = "gold_glue_script.py"
 glue_iam_role = "vg-glue-role"
 delta_core_jar_path = "s3://vg-lakehouse/delta_jar/delta-core_2.12-2.1.0.jar"
 delta_storage_jar_path = "s3://vg-lakehouse/delta_jar/delta-storage-2.1.0.jar"
-glue_args = {
-            "GlueVersion": "4.0", 
-            "NumberOfWorkers": 2, 
-            "Command" : {
-                'PythonVersion': '3'
-                },
-            "DefaultArguments":{
-                '--extra-jars': f'{delta_core_jar_path},{delta_storage_jar_path}',
-                '--extra-py-files' : f'{delta_core_jar_path},{delta_storage_jar_path}', 
-                },
-            "WorkerType": "G.1X"
-        }
-
-# job_run_args = {
-#         "--extra-py-files": f'{delta_core_jar_path},{delta_storage_jar_path}',
-#         "--extra-jars": f'{delta_core_jar_path},{delta_storage_jar_path}'
-# }
-
 glue_script_directory = "/opt/airflow/dags/glue-spark"
-
-
 
 @dag(
     start_date=datetime(2024, 4, 8),  
@@ -42,14 +21,12 @@ glue_script_directory = "/opt/airflow/dags/glue-spark"
     schedule_interval="0 20 * * *",  
     tags=["lakehouse", "glue"],
     default_args = {
-        "retries":2,
-        "retry_delay":timedelta(minutes=5),
+        "retries": 2,
+        "retry_delay": timedelta(minutes=5),
     }
 )
 def lakehouse_dag():
-    @task_group(group_id='glue_scripts_to_S3',
-                default_args={"aws_conn_id": "aws_conn"}
-                )
+    @task_group(group_id='glue_scripts_to_S3')
     def task_group_upload_toS3():
         upload_bronze_job_s3 = LocalFilesystemToS3Operator(
             task_id="upload_bronze_job_to_s3",
@@ -77,36 +54,33 @@ def lakehouse_dag():
 
         upload_bronze_job_s3 >> upload_silver_job_s3 >> upload_gold_job_s3
 
-
-
-    @task_group(group_id='run_glue_jobs',
-                default_args={"aws_conn_id": "aws_conn", 
-                              "iam_role_name": glue_iam_role ,
-                              "create_job_kwargs" : glue_args,
-                              "s3_bucket" : glue_bucket,
-                            #   "script_args" : job_run_args
-                            }
-                )
+    @task_group(group_id='run_glue_jobs')
     def task_group_run_job():
-        submit_glue_bronze_job = AwsGlueJobOperator(
-            task_id="bronze-layer-job",
-            job_name=bronze_glue_job,
-            script_location=f"s3://{glue_bucket}/{bronze_glue_job_key}"
+        create_bronze_glue_job = BashOperator(
+            task_id="create_bronze_glue_job",
+            bash_command=f"""aws glue create-job --name {bronze_glue_job} --role {glue_iam_role} \ 
+                            --command 'python3 {bronze_glue_job_key}' --default-arguments 'GlueVersion=4.0,NumberOfWorkers=2,WorkerType=G.1X, \
+                            --extra-jars={delta_core_jar_path},{delta_storage_jar_path},--extra-py-files={delta_core_jar_path},{delta_storage_jar_path}'""",
+            env={'AWS_DEFAULT_REGION': 'eu-west-3'}
         )
 
-        submit_glue_silver_job = AwsGlueJobOperator(
-            task_id="silver-layer-job",
-            job_name=silver_glue_job,
-            script_location=f"s3://{glue_bucket}/{silver_glue_job_key}"
+        create_silver_glue_job = BashOperator(
+            task_id="create_silver_glue_job",
+            bash_command=f"""aws glue create-job --name {silver_glue_job} --role {glue_iam_role} \ 
+                            --command 'python3 {silver_glue_job_key}' --default-arguments 'GlueVersion=4.0,NumberOfWorkers=2,WorkerType=G.1X,\
+                            --extra-jars={delta_core_jar_path},{delta_storage_jar_path},--extra-py-files={delta_core_jar_path},{delta_storage_jar_path}'""",
+            env={'AWS_DEFAULT_REGION': 'eu-west-3'}
         )
 
-        submit_glue_gold_job = AwsGlueJobOperator(
-            task_id="gold-layer-job",
-            job_name=gold_glue_job,
-            script_location=f"s3://{glue_bucket}/{gold_glue_job_key}"
+        create_gold_glue_job = BashOperator(
+            task_id="create_gold_glue_job",
+            bash_command=f"""aws glue create-job --name {gold_glue_job} --role {glue_iam_role} \ 
+                            --command 'python3 {gold_glue_job_key}' --default-arguments 'GlueVersion=4.0,NumberOfWorkers=2,WorkerType=G.1X,\
+                            --extra-jars={delta_core_jar_path},{delta_storage_jar_path},--extra-py-files={delta_core_jar_path},{delta_storage_jar_path}'""",
+            env={'AWS_DEFAULT_REGION': 'eu-west-3'}
         )
-        submit_glue_bronze_job >> submit_glue_silver_job >> submit_glue_gold_job
 
+        create_bronze_glue_job >> create_silver_glue_job >> create_gold_glue_job
 
     task_group_upload_toS3() >> task_group_run_job()
 
